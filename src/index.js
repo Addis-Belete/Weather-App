8c51ca7cf3d954fc750c268071f486b065e998ad
Addis Belete <addis.bel@gmail.com>
2021-06-25 16:21:17 +0300

    Add a method fetch to get data from weathermap

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
-import _ from 'lodash';
+const cityWeather = function (city_name) {
+	fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city_name}&appid=665d996c0995235e2a6538dc312d1922`, {
+		method: 'GET',
+		mode: 'cors'
+	})
+		.then(function (response) {
+			if (!response.ok) {
+				throw Error(response.statusText + ' - ' + response.url);
+			}
+			console.log(response);
+			return response.json()
 
-function component() {
-	const element = document.createElement('div');
-
-	// Lodash, currently included via a script, is required for this line to work
-	element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-	return element;
+		}).then(function (result) {
+			console.log(result);
+		}).catch((e) => {
+			console.log(e);
+		});
 }
 
-document.body.appendChild(component());
+
+const city_name = document.getElementById('city_name');
+const form = document.querySelector('form');
+form.addEventListener('submit', (e) => {
+	e.preventDefault();
+	if (city_name.value === '') {
+		const errorMessage = document.createElement('div')
+		errorMessage.innerHTML = "Please Fill city name";
+		document.body.append(errorMessage);
+	}
+	else {
+		cityWeather(city_name.value);
+	}
+})
