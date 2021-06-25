a4d6fad38192ce347830a3bcd10fec41f836e078
Addis Belete <addis.bel@gmail.com>
2021-06-25 22:00:02 +0300

    Add style to form

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
+import './style.css'
 import { displayData } from './dom'
+import { clearField } from './logic';
 const cityWeather = function (city_name = 'Addis Ababa') {
 	fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city_name}&appid=665d996c0995235e2a6538dc312d1922`, {
 		method: 'GET',
@@ -19,18 +21,20 @@ const cityWeather = function (city_name = 'Addis Ababa') {
 		});
 }
 
+window.onload = function () {
+	const city_name = document.getElementById('city_name');
+	const form = document.querySelector('form');
+	form.addEventListener('submit', (e) => {
+		e.preventDefault();
+		if (city_name.value === '') {
+			const errorMessage = document.createElement('div')
+			errorMessage.innerHTML = "Please Fill city name";
+			document.body.append(errorMessage);
 
-const city_name = document.getElementById('city_name');
-const form = document.querySelector('form');
-form.addEventListener('submit', (e) => {
-	e.preventDefault();
-	if (city_name.value === '') {
-		const errorMessage = document.createElement('div')
-		errorMessage.innerHTML = "Please Fill city name";
-		document.body.append(errorMessage);
-
-	}
-	else {
-		cityWeather(city_name.value);
-	}
-})
+		}
+		else {
+			cityWeather(city_name.value);
+			clearField()
+		}
+	})
+}
---
+++ src/logic.js
@@ -0,0 +1,8 @@
+
+
+const clearField = function () {
+	const city_name = document.getElementById('city_name');
+	city_name.value = '';
+
+}
+export { clearField }
