815c85c5a51ee6bc59fa9b24ceb125cf23807aba
Addis Belete <addis.bel@gmail.com>
2021-06-28 10:51:23 +0300

    Fix style error

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,22 @@
 const path = require('path');
 
 module.exports = {
-	mode: 'development',
-	entry: './src/index.js',
-	devtool: 'inline-source-map',
-	devServer: {
-		contentBase: './dist',
-	},
-	output: {
-		filename: 'bundle.js',
-		path: path.resolve(__dirname, 'dist'),
-	},
-	module: {
-		rules: [
-			{
-				test: /\.css$/i,
-				use: ['style-loader', 'css-loader'],
-			},
-		],
-	},
+  mode: 'development',
+  entry: './src/index.js',
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
 };
