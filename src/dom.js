c88183aae632226b734e5e00d18b9931eafc6b8e
Addis Belete <addis.bel@gmail.com>
2021-06-28 09:22:05 +0300

    Add a button to toggle btn celcuis and ferheniet and add ES6 arrow

--- src/dom.js
+++ src/dom.js
@@ -1,32 +1,71 @@
 /* eslint-disable camelcase */
 /* eslint-disable func-names */
-const displayData = function (result) {
-  const cityName = document.querySelector('h2');
-  const description = document.querySelector('h4');
-  const temperature = document.querySelector('p');
-  const feels = document.querySelector('.feels');
-  const min_temp = document.querySelector('.temp_min');
-  const max_temp = document.querySelector('.temp_max');
-  const humidity = document.querySelector('.humidity');
-  const icons = document.querySelector('img');
-  icons.src = `https://openweathermap.org/img/w/${result.weather[0].icon}.png`;
-  description.innerHTML = result.weather[0].main;
-  temperature.innerHTML = `${result.main.temp} ℃`;
-  icons.innerHTML = result.weather[0].icon;
-  cityName.innerHTML = result.name;
-  feels.innerHTML = `Feels_like:${result.main.feels_like} ℃ | `;
-  min_temp.innerHTML = `Temp_min:${result.main.temp_min} ℃ | `;
-  max_temp.innerHTML = `Temp_max:${result.main.temp_max} ℃ | `;
-  humidity.innerHTML = `Humidity:${result.main.humidity}`;
+import { toFreneheit } from './logic'
+const displayData = (result) => {
+	const cityName = document.querySelector('h2');
+	const description = document.querySelector('h4');
+	const temperature = document.querySelector('p');
+	const feels = document.querySelector('.feels');
+	const min_temp = document.querySelector('.temp_min');
+	const max_temp = document.querySelector('.temp_max');
+	const humidity = document.querySelector('.humidity');
+	const icons = document.querySelector('img');
+	icons.src = `https://openweathermap.org/img/w/${result.weather[0].icon}.png`;
+	description.innerHTML = result.weather[0].main;
+	temperature.innerHTML = `${result.main.temp} ℃`;
+	icons.innerHTML = result.weather[0].icon;
+	cityName.innerHTML = result.name;
+	feels.innerHTML = `Feels_like:${result.main.feels_like} ℃ | `;
+	min_temp.innerHTML = `Temp_min:${result.main.temp_min} ℃ | `;
+	max_temp.innerHTML = `Temp_max:${result.main.temp_max} ℃ | `;
+	humidity.innerHTML = `Humidity:${result.main.humidity}`;
 };
 
-const displayError = function () {
-  const errorMessage = document.createElement('div');
-  errorMessage.innerHTML = '<p id = "error">Please enter correct city name</p>';
-  document.body.append(errorMessage);
-  setTimeout(() => {
-    errorMessage.innerText = '';
-  }, 3000);
+const displayError = () => {
+	const errorMessage = document.createElement('div');
+	errorMessage.innerHTML = '<p id = "error">Please enter correct city name</p>';
+	document.body.append(errorMessage);
+	setTimeout(() => {
+		errorMessage.innerText = '';
+	}, 3000);
 };
 
-export { displayData, displayError };
+const displayDataInFerheneit = (result) => {
+	const cityName = document.querySelector('h2');
+	const description = document.querySelector('h4');
+	const temperature = document.querySelector('p');
+	const feels = document.querySelector('.feels');
+	const min_temp = document.querySelector('.temp_min');
+	const max_temp = document.querySelector('.temp_max');
+	const humidity = document.querySelector('.humidity');
+	const icons = document.querySelector('img');
+	icons.src = `https://openweathermap.org/img/w/${result.weather[0].icon}.png`;
+	description.innerHTML = result.weather[0].main;
+	temperature.innerHTML = `${toFreneheit(result.main.temp)} ℉`;
+	icons.innerHTML = result.weather[0].icon;
+	cityName.innerHTML = result.name;
+	feels.innerHTML = `Feels_like:${toFreneheit(result.main.feels_like)} ℉ | `;
+	min_temp.innerHTML = `Temp_min:${toFreneheit(result.main.temp_min)} ℉ | `;
+	max_temp.innerHTML = `Temp_max:${toFreneheit(result.main.temp_max)} ℉ | `;
+	humidity.innerHTML = `Humidity:${result.main.humidity}`;
+
+
+}
+
+const changeToCelcius = (result) => {
+	const btn = document.getElementById('celcius')
+	btn.addEventListener('click', () => {
+		displayData(result)
+
+	})
+
+}
+
+const changeToFerheneit = (result) => {
+	const btn = document.getElementById('degree');
+	btn.addEventListener('click', () => {
+		displayDataInFerheneit(result);
+	})
+}
+
+export { displayData, displayError, changeToCelcius, changeToFerheneit };
--- src/logic.js
+++ src/logic.js
@@ -1,5 +1,12 @@
 /* eslint-disable camelcase */
-export default function clearField() {
-  const city_name = document.getElementById('city_name');
-  city_name.value = '';
+const clearField = () => {
+	const city_name = document.getElementById('city_name');
+	city_name.value = '';
 }
+
+const toFreneheit = (value) => {
+	return Math.floor(value * 1.8 + 32)
+}
+
+export { clearField, toFreneheit }
+
